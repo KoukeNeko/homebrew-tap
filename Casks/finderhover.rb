7cc1bd0b40225e04e3ecbb5241b8b3c38b19423f
陳德生 <doeshing@gmail.com>
2025-11-11 14:38:13 +0800

    Update SHA256 for FinderHover cask
    Updated SHA256 checksum for FinderHover cask.

--- Casks/finderhover.rb
+++ Casks/finderhover.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "finderhover" do
   version "1.3.1"
-  sha256 "fe66c67f587cd350df64426e4598cbcd267e0f1e51e328cfc610c8a365f462f7"
+  sha256 "d1de1b9a6612899ddb8166db09f0fd924efa6a7e993682df6c805de4b6b9d339"
 
   url "https://github.com/KoukeNeko/FinderHover/releases/download/v#{version}/FinderHover.app.zip"
   name "FinderHover"
