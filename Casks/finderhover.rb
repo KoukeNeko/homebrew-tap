039a94bbf59904d7643b986050becebce8888b23
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-11-11 12:51:23 +0800

    Add FinderHover cask

---
+++ Casks/finderhover.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "finderhover" do
+  version "1.3.1"
+  sha256 "fe66c67f587cd350df64426e4598cbcd267e0f1e51e328cfc610c8a365f462f7"
+
+  url "https://github.com/KoukeNeko/FinderHover/releases/download/v#{version}/FinderHover.app.zip"
+  name "FinderHover"
+  desc "Show file information on hover in Finder"
+  homepage "https://github.com/KoukeNeko/FinderHover"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "FinderHover.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/FinderHover.app"]
+  end
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: [
+    "~/Library/Preferences/dev.doeshing.FinderHover.plist",
+  ]
+end
