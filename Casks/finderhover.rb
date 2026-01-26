67f00605411b31261df012241c62767258e16a1e
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2026-01-26 16:03:43 +0800

    finderhover: update to v1.6.5

--- Casks/finderhover.rb
+++ Casks/finderhover.rb
@@ -2,8 +2,8 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "finderhover" do
-  version "1.6.4"
-  sha256 "549047ba813c353a99ce8b069d4f7a85b43dfa7bb408977726afa07b5d610e60"
+  version "1.6.5"
+  sha256 "055552a8846894eac6d1d4874dcea966547c3e52844775e142c249353f083fe0"
 
   url "https://github.com/KoukeNeko/FinderHover/releases/download/v#{version}/FinderHover.app.zip"
   name "FinderHover"
