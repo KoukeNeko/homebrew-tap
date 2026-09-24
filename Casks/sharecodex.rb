803037372f6a43c85094b3128aa1c845c8a2c3dc
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2026-09-24 23:55:17 +0800

    sharecodex v0.1.2

---
+++ Casks/sharecodex.rb
@@ -0,0 +1,26 @@
+cask "sharecodex" do
+  version "0.1.2"
+  sha256 "21ea701fef51b174a13015e0b3b1b0fd9ef9b452c0df0c198c622211af2bfb95"
+
+  url "https://github.com/KoukeNeko/ShareCodex/releases/download/v#{version}/ShareCodex-macos-universal.zip"
+  name "ShareCodex"
+  desc "Track shared Claude Code and Codex subscription quota"
+  homepage "https://github.com/KoukeNeko/ShareCodex"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :monterey
+
+  app "ShareCodex.app"
+
+  uninstall launchctl: "io.github.koukeneko.sharecodex",
+            quit:      "io.github.koukeneko.sharecodex"
+
+  zap trash: [
+    "~/Library/Application Support/ShareCodex",
+    "~/Library/LaunchAgents/io.github.koukeneko.sharecodex.plist",
+  ]
+end
